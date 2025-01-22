13957fcebfb39f8fda13d71502c018e94c2b2bbe
Nithin <nithinv7775@gmail.com>
2025-01-22 15:53:13 +0530

    updated for Deployment

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/userDetails")
-    .then(() => {
-        console.log("Mongo Connected");
-    })
-    .catch(() => {
-        console.log("Mongo Not Connected");
-    });
+// mongoose.connect("mongodb://localhost:27017/userDetails")
+//     .then(() => {
+//         console.log("Mongo Connected");
+//     })
+//     .catch(() => {
+//         console.log("Mongo Not Connected");
+//     });
 
-const mySchema = mongoose.Schema({
-    user:{
-        type:String,
-        required:true
-    },
-    password: {
-        type: String,
-        required:true
-    }
-});
+// const mySchema = mongoose.Schema({
+//     user:{
+//         type:String,
+//         required:true
+//     },
+//     password: {
+//         type: String,
+//         required:true
+//     }
+// });
 
 const user = mongoose.model("user", mySchema);
 
@@ -39,39 +39,42 @@ app.post("/signin", (req, res) => {
     // const pwd = req.body.pwd;
     const {username, password} = req.body;
 
-    const details = {
-        user: username,
-        password: password
-    };
-    const userDetails = new user(details);
-    userDetails.save()
-    .then((response) => {
-    res.send("Successfully Signed In");
-    })
-    .catch(()=>{
-        res.send("Failed to sign in");
-    })
+    res.send(" Dumy Successfully Signed In");
+
+    // const details = {
+    //     user: username,
+    //     password: password
+    // };
+    // const userDetails = new user(details);
+    // userDetails.save()
+    // .then((response) => {
+    // res.send("Successfully Signed In");
+    // })
+    // .catch(()=>{
+    //     res.send("Failed to sign in");
+    // })
 });
 
 app.post("/login", (req, res) => {
     const {username, password} = req.body.user;
+    res.send("MongoDB not connected");
 
-    user.findOne({ user: username })
-    .then((response)=>{
-        if(response.user === username)
-        {
-            if(response.password === password)
-            {
-                res.send({ success: true });
-            }
-            else{
-                res.send({ success: false });
-            }
-        }
-    })
-    .catch(()=>{
-        res.send("User Not Found or the username is invalid");
-    })
+    // user.findOne({ user: username })
+    // .then((response)=>{
+    //     if(response.user === username)
+    //     {
+    //         if(response.password === password)
+    //         {
+    //             res.send({ success: true });
+    //         }
+    //         else{
+    //             res.send({ success: false });
+    //         }
+    //     }
+    // })
+    // .catch(()=>{
+    //     res.send("User Not Found or the username is invalid");
+    // })
     // .catch((err)=>{
     //     res.send(err);
     // })
